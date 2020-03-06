66cc8368585f121fc66823019aa3d6445aae221c
Satish T <tosatishs1693@gmail.com>
2020-03-07 01:27:05 +0530

    Commit version 2

---
+++ baker/src/DataBaseConnection.java
@@ -0,0 +1,33 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DataBaseConnection {
+	Connection con = null;
+	public Connection createConnection()
+	{
+		
+		try {
+			
+	        
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+	        
+			System.out.println("Driver established connection");
+
+			con = DriverManager.getConnection("jdbc:oracle:thin:@172.16.153.233:1521:xe","user2","techm123");
+			System.out.println("Connection to database established successfully");
+	}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println(" Error Connecting with database");
+		}
+
+		return con;
+		
+}
+
+}
+
